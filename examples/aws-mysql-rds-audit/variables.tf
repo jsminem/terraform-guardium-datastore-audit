25daaf7bc105ba0144bc0de2244e25dcc8c63f2a
Jasmine Multani <jasmine@ibm.com>
2025-12-02 11:27:34 -0500

    update description
    Signed-off-by: Jasmine Multani <jasmine@ibm.com>

--- examples/aws-mysql-rds-audit/variables.tf
+++ examples/aws-mysql-rds-audit/variables.tf
@@ -73,7 +73,7 @@ variable "audit_query_log_limit" {
 
 variable "cloudwatch_logs_exports" {
   type        = list(string)
-  description = "List of log types to export to CloudWatch. Valid values for MySQL: audit, error, general, slowquery"
+  description = "List of log types to export to CloudWatch. Valid values for MySQL: audit, error"
   default     = ["audit", "error"]
 }
 
